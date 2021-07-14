80e44a7edc3b5703ebb36233550dad7863f7e133
Tunkie1 <87043281+Tunkie1@users.noreply.github.com>
2021-07-14 19:20:11 -0400

    Update edit.blade.php

--- resources/views/blogs/edit.blade.php
+++ resources/views/blogs/edit.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app1')
+
 @section('content')
+<div class="container">
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
@@ -10,75 +12,57 @@
             </div>
         </div>
     </div>
-   
+
     @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Warning!</strong> Please check input field code<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
+    <div class="alert alert-danger">
+        <strong>Warning!</strong> Please check input field code<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
     @endif
-  
+
     <form action="{{ route('blogs.update',$blog->id) }}" method="POST">
         @csrf
         @method('PUT')
-   
-         <div class="row">
+
+        <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Nombre:</strong>
-                    <input type="text" name="title" value="{{ $blog->title }}" class="form-control" placeholder="Nombre">
+                    <input type="text" name="title" value="{{ $blog->title }}" class="form-control"
+                        placeholder="Nombre">
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Descripcion:</strong>
-                    <textarea class="form-control" style="height:150px" name="description" placeholder="Escriba aqui la descripcion del producto">{{ $blog->description }}</textarea>
+                    <textarea class="form-control" style="height:150px" name="description"
+                        placeholder="Escriba aqui la descripcion del producto">{{ $blog->description }}</textarea>
                 </div>
             </div>
-             <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>precio:</strong>
-                    <input type="number" name="precio" class="form-control" value="{{ $blog->precio }}" placeholder="Ingrese el precio del producto">
+                    <input type="number" name="precio" class="form-control" value="{{ $blog->precio }}"
+                        placeholder="Ingrese el precio del producto">
                 </div>
             </div>
 
-             <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Imagen (URL):</strong>
-                    <input type="text" name="url" class="form-control" value="{{ $blog->url }}" placeholder="Ingrese URL de la imagen">
+                    <input type="text" name="url" class="form-control" value="{{ $blog->url }}"
+                        placeholder="Ingrese URL de la imagen">
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-              <button type="submit" class="btn btn-primary">Subir</button>
+                <button type="submit" class="btn btn-primary">Subir</button>
             </div>
         </div>
-   
+
     </form>
+</div>
 @endsection
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PRODUCTOS M&S INGENIERIAS</title>
-    <meta name="Nova theme" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/png" href="assets/images/logouwu.png"/>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css"/>
-    <link rel="stylesheet" href="assets/css/responsive.css"/>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
-
-</head>
-</html>
